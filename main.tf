6db65a091bd3543724aee44125a1c7bac5f84756
mental1234 <ramonesparza1234@gmail.com>
2019-02-01 10:22:08 -0600

    new name ssh key

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ resource "aws_instance" "base_instance" {
 }
 
 resource "aws_key_pair" "user" {
-   key_name = "ramon"
+   key_name = "ramon2"
    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBGnDm1JzbglK4qOO9U5noHAAfA7+aMQ65Irxiyasq6RBGoB1VVCTUsXhDZMx7kjWRAOG+A07j7gFJIoaq2iEVJiz5896aLyV8lv4bw+r3Y6z0NVuOH8SryAWhy0UnyN0CfNru842e18jSWwVMB+jzEnqo2p5kpGa2DCQHgA2+I2I+tahsZZQQ/rkDRyuEwvIIhwq1g9Yyno/V1G19HwuLVjleBObcccOBOdK6Q5ZppCrIP6TY97Qj5rm+LF6R2ejkLOvTj4LHyMIOXPW08wUASsraSr0nzYoGvL05/x2Pl6oG+E6mHR2b9JrKheAMIKTNB8TV2yZDw/PAu8klE+CB rodriguez.esparza.ramon@gmail.com"
 }
